f41bbdccacd74a3c559a4fe8609a99c390c9bfe4
Lebot Apps <lebot.llc@gmail.com>
2021-05-16 14:09:18 -0400

    lab update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("JS is working");
+//alert("JS is working");
+
+//Select and cache <main> in a variable named mainEl
+let mainEl = document.querySelector("main");
+
+// Set background color of mainEl
+mainEl.style.background = 'var(--main-bg)';
+
+//Set content of mainEl to <h1>
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+
+//Add a class of flex-ctrto mainEl
